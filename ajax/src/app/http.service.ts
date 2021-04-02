405c1544de85192aa69ee4946b9d856fb8d434a9
teddy-clark <edwardeclarkiii@gmail.com>
2021-04-02 18:01:33 -0400

    update app

---
+++ ajax/src/app/http.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  private apiserver = "http://localhost:3000/";
+
+  constructor(private httpClient: HttpClient) { }
+
+  public sendGetRequest(endpoint: string) {
+    let finaldest = "http://localhost:3000" + endpoint
+    return this.httpClient.get(finaldest);
+  }
+}
